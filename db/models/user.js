0b5de66639cb8a0aa4ab3fa157565f18bec9cdb5
khwanchanok <khwanchanoksriphattharasith@gmail.com>
2023-04-10 18:06:01 +0700

    Test unfinish

---
+++ db/models/user.js
@@ -0,0 +1,34 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    'User',
+    {
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        unique: true,
+        validate: {
+          isEmail: true,
+        },
+      },
+      mobile: {
+        type: DataTypes.STRING,
+        unique: true,
+        validate: {
+          is: /^[0-9]{10}$/,
+        },
+      },
+    },
+    {
+      underscored: true,
+    },
+  );
+
+  return User;
+};
